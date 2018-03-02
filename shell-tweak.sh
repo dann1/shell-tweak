83ff24b12bc988138171511392f690da839d8a3c
dann1 <dann1telecom@gmail.com>
2018-03-02 15:46:45 -0500

    removed hardcode source

--- shell-tweak.sh
+++ shell-tweak.sh
@@ -3,14 +3,13 @@
 # Sample script for installing
 
 cd ~
-rsync -av --progress dann1@192.168.2.1:~/Projects/shell-tweak/ .shell-tweak
+mv  shell-tweak/ .shell-tweak
 
 if [[ $? -eq 0 ]]; then
         mv .bash_history bash_history
         rm .bash*
         ln -s ~/.shell-tweak/.* .
         mv bash_history .bash_history
-        rm ~/shell-tweak.sh
 fi
 
 cd -
